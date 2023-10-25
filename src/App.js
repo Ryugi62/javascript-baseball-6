7df01badb15e15d058afdf5c7d7f374584e869d6
Ryugi62 <xorjf1027@naver.com>
2023-10-25 11:12:53 +0900

    계속되는 테스트 실패로 코드 처음부터 다시

--- src/App.js
+++ src/App.js
@@ -1,83 +1,23 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
-    // 게임 시작
-    this.isPlaying = true; // 게임 플레이 여부
-    this.isCollect = true; // 문제를 맞췄는지 여부
-    this.computer = []; // 컴퓨터가 생성한 수
-    this.user = []; // 사용자가 입력한 수
+  constructor() {
+    this.isPlaying = false;
+    this.isCorrect = false;
+    this.computer = "";
+    this.user = "";
     this.status = {
-      // 게임 상태
-      strike: 0, // 스트라이크
-      ball: 0, // 볼
-      out: 0, // 아웃
+      strike: 0,
+      ball: 0,
+      out: 0,
     };
 
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-
-    while (this.isPlaying) {
-      // 컴퓨터가 랜덤한 수 3개를 생성
-      if (this.isCollect) this.computer = this.generateComputer();
-
-      // 사용자가 3개의 수를 입력
-      this.user = await this.inputUserNumbers();
-
-      // 컴퓨터가 생성한 수와 사용자가 입력한 수를 비교
-      this.isCollect = this.compareNumbers();
-
-      this.showResult();
-
-      // 문제를 맞췄다면 재시작 여부를 물어봄
-      if (this.isCollect) {
-        this.isPlaying = await this.restart();
-      }
-    }
+    this.init();
   }
 
-  generateComputer() {
-    // 1 ~ 9 사이의 서로 다른 수 3개를 랜덤하게 생성
-    const NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3); // 서로 다른 수 3개를 랜덤하게 생성
-
-    return NUMBERS;
-  }
-
-  async inputUserNumbers() {
-    const NUMBERS = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    ).then((input) => input.split("").map((number) => parseInt(number)));
-
-    if (NUMBERS.length !== 3) throw new Error("[ERROR]");
-
-    return NUMBERS;
-  }
-
-  compareNumbers() {
-    const COMPUTER = [...this.computer];
-    const USER = [...this.user];
-
-    // strike, ball, out 초기화
-    this.clearStatus();
-
-    // strike, ball, out 계산
-    for (let i = 0; i < USER.length; i++) {
-      if (COMPUTER[i] === USER[i]) {
-        this.status.strike++;
-        continue;
-      }
-
-      if (COMPUTER.includes(USER[i])) {
-        this.status.ball++;
-        continue;
-      }
-
-      this.status.out++;
-    }
-
-    return this.status.strike === 3;
-  }
-
-  clearStatus() {
+  init() {
+    this.isPlaying = true;
+    this.isCorrect = false;
     this.status = {
       strike: 0,
       ball: 0,
@@ -85,35 +25,36 @@ class App {
     };
   }
 
-  showResult() {
-    const STRIKE = this.status.strike;
-    const BALL = this.status.ball;
-    const OUT = this.status.out;
-
-    let message = "";
-    message += BALL > 0 ? `${BALL}볼 ` : "";
-    message += STRIKE > 0 ? `${STRIKE}스트라이크` : "";
-
-    message = OUT === 3 ? "낫싱" : message;
-
-    MissionUtils.Console.print(message);
+  play() {
+    while (this.isPlaying) {
+      this.computer = this.generateComputer();
+      this.user = this.getInput();
+    }
   }
 
-  async restart() {
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    MissionUtils.Console.print(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-    );
-    const IS_RESTART = await MissionUtils.Console.readLineAsync("");
+  generateComputer() {
+    const COMPUTER = [];
+    const MIN = 1;
+    const MAX = 9;
 
-    if (IS_RESTART === "1") return true;
-    if (IS_RESTART === "2") return false;
+    while (COMPUTER.length < 3) {
+      const NUMBER = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
 
-    throw new Error("[ERROR] 1 또는 2를 입력해주세요.");
+      if (!COMPUTER.includes(NUMBER)) COMPUTER.push(NUMBER);
+    }
+
+    return COMPUTER.join("");
+  }
+
+  getInput() {
+    const INPUT = MissionUtils.Console.readLineAsync();
+
+    if (!this.isValidInput(INPUT)) throw new Error("Invalid Input");
+
+    return INPUT;
+  }
+
+  isValidInput(INPUT) {
+    return INPUT.length === 3 && Number.isInteger(INPUT);
   }
 }
-
-const app = new App();
-app.play();
-
-export default App;
